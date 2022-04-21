b49133aecb61d76b18808b8f190431534b3648ad
Crawford Vining <crawpdx@gmail.com>
2022-04-21 08:54:02 -0700

    added loader

--- src/Image.js
+++ src/Image.js
@@ -1,28 +1,31 @@
 import React, {useState, useEffect} from 'react'
 import key from './key/key'
+import { FaRocket } from 'react-icons/fa';
 
 export default function Image(){
     const [imageInfo, setImageInfo] = useState([])
+    const [loading, setLoading] = useState(false)
 
    useState(async () => {
+       setLoading(true)
         await fetch(`https://api.nasa.gov/planetary/apod?api_key=${key}`)
         .then(response => response.json())
         .then(data => setImageInfo(data))
-        
+        setLoading(false)
     }, [])
 
     console.log(imageInfo)
-    
+   
     return(
         <div style={{ margin: '0 auto', width: '30em'}}>
             <p><strong>{imageInfo.title}</strong></p>
-            <img style={{ 
+           {loading ? <FaRocket /> : <img style={{ 
                 width: '500px', 
                 height: 'auto',
                 borderRadius: '10px',
                 boxShadow: '3px 3px 5px #4a4a4a'
-                }} src={imageInfo.hdurl} />
+                }} src={imageInfo.hdurl} />}
             <p style={{textAlign: 'left'}}>{imageInfo.explanation}</p>
-        </div>
+         </div>
     ) 
 }
