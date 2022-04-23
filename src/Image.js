ec79b898ac24a8489c8ba127e22e8da3d58c0e7f
Crawford Vining <crawpdx@gmail.com>
2022-04-23 10:14:19 -0700

    added responsive styling and animation

--- src/Image.js
+++ src/Image.js
@@ -17,15 +17,33 @@ export default function Image(){
     console.log(imageInfo)
    
     return(
-        <div style={{ margin: '0 auto', width: '30em'}}>
-            <p><strong>{imageInfo.title}</strong></p>
+        <div style={{ margin: '0 auto', maxWidth: '30em', margin: '1em auto'}}>
+            <h2 style={{
+                opacity: '0',
+                transform: 'translateY(2em)',
+                animation: 'fadeIn 1s .5s forwards'
+            }}><strong>{imageInfo.title}</strong></h2>
            {loading ? <FaRocket /> : <img style={{ 
-                width: '500px', 
+                display: 'block',
+                maxWidth: '100%', 
                 height: 'auto',
+                margin: '0 auto',
                 borderRadius: '10px',
-                boxShadow: '3px 3px 5px #4a4a4a'
+                boxShadow: '3px 3px 5px #4a4a4a',
+                opacity: '0',
+                transform: 'translateY(2em)',
+                animation: 'fadeIn 1s 1s forwards'
                 }} src={imageInfo.hdurl} />}
-            <p style={{textAlign: 'left'}}>{imageInfo.explanation}</p>
+            <p style={{
+                maxWidth: '100%',
+                padding: '1em',
+                borderRadius: '8px',
+                boxShadow: '3px 3px 5px #4a4a4a',
+                background: 'grey',
+                textAlign: 'left',
+                opacity: '0',
+                transform: 'translateY(2em)',
+                animation: 'fadeIn 1s 2s forwards'}}>{imageInfo.explanation}</p>
          </div>
     ) 
 }
